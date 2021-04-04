2abf7101b6235c73cd2f6ea5d079bdcfd28eee8c
Jeremy Marchandeau <jeremy@tigerandjune.com>
2021-04-04 16:21:37 +0200

    TGM activation plugin added

--- includes/timber.php
+++ includes/timber.php
@@ -11,29 +11,6 @@ else {
     require_once(plugin_dir_path( __DIR__ ) . '/vendor/autoload.php');
 } 
 
-/**
- * This ensures that Timber is loaded and available as a PHP class.
- * If not, it gives an error message to help direct developers on where to activate
- */
-if ( ! class_exists( 'Timber' ) ) {
-
-    add_action(
-        'admin_notices',
-        function() {
-            echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
-        }
-    );
-
-    add_filter(
-        'template_include',
-        function( $template ) {
-            return get_stylesheet_directory() . '/static/no-timber.html';
-        }
-    );
-    return;
-}
-
-
 
 $timber = new \Timber\Timber();
 
