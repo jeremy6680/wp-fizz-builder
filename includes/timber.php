21c8e1641deaccdd4bf1d2c067ab4577bf5cafcf
Jeremy Marchandeau <jeremy@tigerandjune.com>
2021-04-04 10:42:10 +0200

    improvements on compatibility

--- includes/timber.php
+++ includes/timber.php
@@ -1,18 +1,51 @@
 <?php
 
 // Load Timber
-require_once(plugin_dir_path( __DIR__ ) . '/vendor/autoload.php');
+
+$filepath = get_template_directory() . '/vendor/autoload.php';
+
+if (file_exists($filepath)) {
+    require_once(get_template_directory() . '/vendor/autoload.php');
+}  
+else {
+    require_once(plugin_dir_path( __DIR__ ) . '/vendor/autoload.php');
+} 
+
+/**
+ * This ensures that Timber is loaded and available as a PHP class.
+ * If not, it gives an error message to help direct developers on where to activate
+ */
+if ( ! class_exists( 'Timber' ) ) {
+
+    add_action(
+        'admin_notices',
+        function() {
+            echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
+        }
+    );
+
+    add_filter(
+        'template_include',
+        function( $template ) {
+            return get_stylesheet_directory() . '/static/no-timber.html';
+        }
+    );
+    return;
+}
+
+
 
 $timber = new \Timber\Timber();
 
 // Set Timber Locations + possibility to override templates in theme
 Timber::$locations = array(
-    plugin_dir_path( __DIR__ ) . '/views',
-    plugin_dir_path( __DIR__ ) . '/components',
+
     get_stylesheet_directory() . '/views',
     get_stylesheet_directory() . '/components',
     get_template_directory() . '/views',
-    get_template_directory() . '/components'
+    get_template_directory() . '/components',
+    plugin_dir_path( __DIR__ ) . '/views',
+    plugin_dir_path( __DIR__ ) . '/components'
 );
 
 //cf https://github.com/timber/timber/issues/248
--- templates/pagebuilder-template.php
+++ templates/pagebuilder-template.php
@@ -10,5 +10,11 @@ $context = Timber::context();
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
 $templates        = array( 'builder.twig' );
+$twigbase = get_template_directory() . '/views/base.twig';
+$file_exists = file_exists($twigbase);
+if($file_exists):
+Timber::render( 'builder.twig', $context );
+else:
+Timber::render( 'notimber-builder.twig', $context );
+endif;
 
-Timber::render( $templates, $context );
