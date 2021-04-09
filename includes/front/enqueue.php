250dfd4dde9eca37f6484417720f9d20a6b3e53b
Jeremy Marchandeau <jeremy.marchandeau@hotmail.fr>
2021-04-09 11:43:22 +0200

    check if dist assets file exists before enqueing

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -1,13 +1,21 @@
 <?php
 
 function wpfb_scripts() {
-    wp_register_style( 'wpfb-global-styles', plugins_url( 'assets/dist/global.css' , WPFBUILDER_PLUGIN_URL ), 100 );
-    wp_register_style( 'wpfb-components-styles', plugins_url( 'assets/dist/components.css' , WPFBUILDER_PLUGIN_URL ), 100 );
-    wp_register_script( 'wpfb_main', plugins_url( '/assets/dist/main.js', WPFBUILDER_PLUGIN_URL ), ['jquery'], '1.0.0', true );
 
-    if(is_page_template('pagebuilder-template.php')) {
-	    wp_enqueue_style( 'wpfb-components-styles' );
-	    wp_enqueue_style( 'wpfb-global-styles' );
-	    wp_enqueue_script( 'wpfb_main' );   	
+	$file = plugin_dir_path(dirname(__FILE__)).'../assets/dist/';
+    
+    if(file_exists($file)) {
+
+    	$dist = plugin_dir_url( __DIR__ ). '../assets/dist/';
+	    wp_register_style( 'wpfb-global-styles', $dist . 'global.css', 100 );
+	    wp_register_style( 'wpfb-components-styles', $dist . 'components.css', 100 );
+	    wp_register_script( 'wpfb_main', $dist . 'main.js', ['jquery'], '1.0.0', true );
+
+	    if(is_page_template('pagebuilder-template.php')) {
+		    wp_enqueue_style( 'wpfb-components-styles' );
+		    wp_enqueue_style( 'wpfb-global-styles' );
+		    wp_enqueue_script( 'wpfb_main' );   	
+	    }
     }
+
 }
